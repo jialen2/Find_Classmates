07c326dd30bf7d599fa13524f0c291fa1d2dd133
jialening <jialen2@illinois.edu>
2019-12-03 00:33:31 -0600

    commit code

---
+++ app/src/main/java/com/example/xinshuo3/createAccount.java
@@ -0,0 +1,13 @@
+package com.example.xinshuo3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class createAccount extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat_account);
+    }
+}
